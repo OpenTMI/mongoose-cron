3115e3122704bf9dd2fbcf456302e5d5ccca6f62
Jussi Vatjus-Anttila <jussiva@gmail.com>
2019-01-12 14:35:01 +0200

    update eslintrc and initial test

--- test/tests.js
+++ test/tests.js
@@ -1,5 +1,14 @@
+const mongoose = require('mongoose');
+const {expect} = require('chai');
+
+const CronPlugin = require('../lib/plugin');
+
+const {Schema} = mongoose;
+
 describe('mongoose-cron', function () {
     it('plugin', function () {
-
+        const MySchema = new Schema({});
+        MySchema.plugin(CronPlugin);
+        expect(MySchema).to.ok;
     });
 });
